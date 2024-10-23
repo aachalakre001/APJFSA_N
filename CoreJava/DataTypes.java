6b8e8a4ac8df73d12824a1f141048dca52cb8aff
Aachal Akre <aachalakre@gmail.com>
2024-10-23 18:17:37 +0530

    Add files via upload

---
+++ CoreJava/DataTypes.java
@@ -0,0 +1,27 @@
+package CoreJava;
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+		int i=10; //-2,147,483,648-2,147,483,648 to 2,147,483,647
+		float f=13.9456f; //upto 7 decimal digits
+		double d=3000000.674555; //upto 16 decimal digits
+		char s='A'; //ASCII values 0 to 255
+		boolean b=true;
+		byte b1=40;
+		short s1=30000; //-32,768 to 32,767
+		long l=20000000;//-9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
+		
+		System.out.println(" All DataTypes are shown below:- ");
+		System.out.println(" 1) Integer= "+i);
+		System.out.println(" 2) Float= "+f);
+		System.out.println(" 3) Double= "+d);
+		System.out.println(" 4) Character= "+s);
+		System.out.println(" 5) Boolean= "+b);
+		System.out.println(" 6) Byte= "+b1);
+		System.out.println(" 7) Short= "+s1);
+		System.out.println(" 8) Long= "+l);
+		
+	}
+
+}
