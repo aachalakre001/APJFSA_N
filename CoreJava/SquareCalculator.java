629cb9bb66fa9479988bdf9a69e9322d27227fe6
Aachal Akre <aachalakre@gmail.com>
2024-10-24 16:03:32 +0530

    SquareCalculator.java

--- CoreJava/SquareCalculator.java
+++ CoreJava/SquareCalculator.java
@@ -2,12 +2,12 @@ package CoreJava;
 
 public class SquareCalculator {
 	public static void main(String[] args) {
-    // Method to calculate area and perimeter {
-		int sideLength = 12; // side length
-       int area = sideLength * sideLength; // Area = side^2
-       int perimeter = 4 * sideLength;      // Perimeter = 4 * side
+    {	//to calculate area and perimeter 
+	int sideLength = 12; // side length
+	int area = sideLength * sideLength; // Area = side^2
+       	int perimeter = 4 * sideLength;      // Perimeter = 4 * side
         System.out.println("side length of a square:"+sideLength);
-       System.out.println("Area of a square is: "+area);
-       System.out.println("Perimeter of a square is: "+perimeter);
+	System.out.println("Area of a square is: "+area);
+	System.out.println("Perimeter of a square is: "+perimeter);
     }
 }
