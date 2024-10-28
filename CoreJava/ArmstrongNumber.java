e4b325535bb35e80281f77ce941cbfa1de315652
Aachal Akre <aachalakre@gmail.com>
2024-10-28 15:52:21 +0530

    Add files via upload

---
+++ CoreJava/ArmstrongNumber.java
@@ -0,0 +1,39 @@
+package CoreJava;
+
+import java.util.Scanner; // Import the Scanner class for user input
+
+public class ArmstrongNumber {
+	public static void main(String[] args) {
+		// Create a Scanner object to read input from the user
+		Scanner scanner = new Scanner(System.in);
+		//user input
+		System.out.print("Enter a number: ");
+		int number = scanner.nextInt(); 
+
+		int originalNumber = number; 
+		int sum = 0; 
+
+		// Calculate the number of digits in the number
+		int numberOfDigits = String.valueOf(number).length();
+
+		// Loop through each digit of the number
+		while (number != 0) {
+			int digit = number % 10; // Get the last digit
+			sum += Math.pow(digit, numberOfDigits); // Raise the digit to the power of numberOfDigits and add to sum
+			number /= 10; // Remove the last digit from the number
+		}
+
+		// Check if the sum of the powers equals the original number
+		if (sum == originalNumber) {
+			System.out.println(originalNumber + " is an Armstrong number.");
+		} else {
+			System.out.println(originalNumber + " is not an Armstrong number.");
+		}
+
+	}
+}
+
+/*output: Enter a number: 254
+254 is not an Armstrong number.
+*/
+ 
---
+++ CoreJava/ReverseNumber.java
@@ -0,0 +1,30 @@
+package CoreJava;
+
+import java.util.Scanner; 
+
+public class ReverseNumber {
+	public static void main(String[] args) {
+		// Create a Scanner object to read input from the user
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.print("Enter a number to reverse: ");
+		int number = scanner.nextInt(); 
+		int reversedNumber = 0; 
+
+		// Use a while loop to reverse the number
+		while (number != 0) {
+			int digit = number % 10; // Get the last digit of the number
+			reversedNumber = reversedNumber * 10 + digit; // Append the digit to the reversed number
+			number = number / 10; // Remove the last digit from the number
+		}
+
+		// Display the reversed number
+		System.out.println("Reversed Number: " + reversedNumber);
+
+
+	}
+}
+/*output:Enter a number to reverse: 543
+Reversed Number: 345*/
+
+
