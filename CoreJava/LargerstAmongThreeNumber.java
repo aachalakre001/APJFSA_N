76fa1af726f65f60d86d98b4d90dcf239e1c2c1e
Aachal Akre <aachalakre@gmail.com>
2024-10-28 15:53:08 +0530

    Add files via upload

---
+++ CoreJava/LargerstAmongThreeNumber.java
@@ -0,0 +1,24 @@
+package CoreJava;
+
+import java.util.Scanner;
+
+public class LargerstAmongThreeNumber {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter three numbers: ");
+		int num1=sc.nextInt();
+		int num2=sc.nextInt();
+		int num3=sc.nextInt();
+		
+		if (num1 >= num2 && num1 >= num3) {
+	          System.out.println("num1 is greater: " + num1);
+	      } else if (num2 >= num1 && num2 >= num3) {
+	          System.out.println("num2 is greater: " + num2);
+	      } else {
+	          System.out.println("num3 is greater: " + num3);
+	      }
+
+	}
+
+}
